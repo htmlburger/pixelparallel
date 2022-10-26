5865255828f81e615755defdfc7e6494fc3eb95b
ypopov <ypopov@htmlbox.net>
2022-10-26 15:56:02 +0300

    Fix previous issue where multiple pixelparallel popups would open on the same page

--- extension/background.js
+++ extension/background.js
@@ -65,35 +65,3 @@ function getActiveTab(cb) {
     cb(tabs[0].id);
   });
 }
-
-
-chrome.manifest = chrome.runtime.getManifest();
-
-var injectIntoTab = function (tab) {
-    // You could iterate through the content scripts here
-    var scripts = chrome.manifest.content_scripts[0].js;
-    for( let i = 0; i < scripts.length; i++ ) {
-        chrome.scripting.executeScript({
-          target: {tabId: tab.id},
-          files: [scripts[i]]
-        });
-    }
-}
-
-// Get all windows
-chrome.windows.getAll({
-    populate: true
-}, function (windows) {
-    var i = 0, w = windows.length, currentWindow;
-    for( ; i < w; i++ ) {
-        currentWindow = windows[i];
-        var j = 0, t = currentWindow.tabs.length, currentTab;
-        for( ; j < t; j++ ) {
-            currentTab = currentWindow.tabs[j];
-
-            if (currentTab.url && !currentTab.url.match(/(chrome):\/\//gi) ) {
-                injectIntoTab(currentTab);
-            }
-        }
-    }
-});
